55d4b77da5c0c1e791b5cfd46e55944320f68d52
aartiQuatics <59826672+aartiQuatics@users.noreply.github.com>
2021-02-09 18:57:38 +0530

    App changes

--- src/App.js
+++ src/App.js
@@ -10,9 +10,9 @@ class App extends React.Component {
       <div className="card">
         <div className="header">
           <div className="logo">
-            <a href=".">LOGO</a>
+            {/* <a href=".">Five Star Placements</a> */}
           </div>
-          <div className="social">
+          {/* <div className="social">
             <a href="https://facebook.com" title="Facebook" target="_blank" rel="noopener noreferrer">
               <IconFacebook className="icon" />
             </a>
@@ -22,20 +22,21 @@ class App extends React.Component {
             <a href="https://github.com/arkn98/coming-soon" title="GitHub" target="_blank" rel="noopener noreferrer">
               <IconGithub className="icon" />
             </a>
-          </div>
+          </div> */}
         </div>
         <div className="content">
           <div className="title-holder">
-            <h1>Get ready for the change.</h1>
-            <p>Website coming soon. Please check back to know more. Shoot us an email if you're curious.</p>
+            <h1>Five Star Placements</h1>
+            <h2>We are coming soon</h2>
+            <p>Website coming soon. Please check back to know more.</p>
           </div>
-          <a href="mailto:user@example.com">
+          {/* <a href="mailto:user@example.com">
             <div className="cta">Send us an email</div>
-          </a>
+          </a> */}
         </div>
-        <div className="footer">
+        {/* <div className="footer">
           <span>made by <a className="underlined" href="https://github.com/arkn98" target="_blank" rel="noopener noreferrer">arkn98</a> using <a className="underlined" href="https://reactjs.org/" title="ReactJS" target="_blank" rel="noopener noreferrer">React</a> | <a className="underlined" href="https://github.com/arkn98/coming-soon" title="GitHub repo" target="_blank" rel="noopener noreferrer">GitHub</a></span>
-        </div>
+        </div> */}
       </div>
     );
   }
